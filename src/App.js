33d9c2cec7147e02ea2358bb49fbd896b299d9e8
jeanvegadev <jcvega.dev@gmail.com>
2024-01-18 12:17:30 -0500

    add menu header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Example from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Example/>
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hola a todos soy Jean Vega.</p>
         <p>Esta es mi sitio web personal. Es construido con React.</p>
-      </header>
+        <h1 className="text-3xl font-bold underline">
+        Hello world!
+        </h1>
+      </div>
     </div>
   );
 }
