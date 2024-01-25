fadc3eb1d0650f43c182850845561abeed238f16
jeanvegadev <jcvega.dev@gmail.com>
2024-01-25 17:15:14 -0500

    text grande all App js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,10 +3,22 @@ import React from 'react';
 
 const Projects = () => {
   return (
-    <div>
-      <h1>Welcome to the Proyectos Page</h1>
-      <p>This is the contact page content of your SPA.</p>
-      {/* Add more content or components as needed */}
+    <div className="container mx-auto items-center justify-center md:justify-between px-6 pt-1 lg:px-8 py-16 sm:py-16">
+      <ul>
+        <li class="my-10 mx-4">
+          <span><span></span></span>
+          <div>
+            <a href="https://github.com/jeanvegadev/personalweb" target="_blank" rel="noopener" class="text-blue-600 hover:underline"><strong>JeanDev Personal Web</strong></a> - <span>This personal web is built with React and Tailwind. It is deployed in Vercel.</span>
+          </div>
+        </li>
+        <li class="my-10">
+            <div id="linkedin-j" class="my-4 mx-4">
+            ...
+          </div>
+        </li>
+      </ul>
+      
+      
     </div>
   );
 };
