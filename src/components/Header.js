22ef8a1932ce4e849dbabbe5424af02b06f79e92
jeanvegadev <jcvega.dev@gmail.com>
2024-04-24 12:11:24 -0500

    fix menumobilebutton

--- src/components/Header.js
+++ src/components/Header.js
@@ -89,10 +89,10 @@ export default function MenuHeader({toggleDarkMode, c1}) {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                <Link to="/" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50">Home</Link>
-                <Link to="/projects" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7  hover:bg-gray-50">Projects</Link>
-                <Link to="/about" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50">About me</Link>
-                <Link to="/contact" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50">Contact</Link>
+                <Link to="/" onClick={() => setMobileMenuOpen(false)}  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50">Home</Link>
+                <Link to="/projects" onClick={() => setMobileMenuOpen(false)}  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7  hover:bg-gray-50">Projects</Link>
+                <Link to="/about" onClick={() => setMobileMenuOpen(false)}  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50">About me</Link>
+                <Link to="/contact" onClick={() => setMobileMenuOpen(false)}  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50">Contact</Link>
               </div>
             </div>
           </div>
