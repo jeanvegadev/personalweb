e612f241fc46f9e73f7779d97410790f389c3c48
jeanvega <jean.vega@companyon.de>
2025-11-21 17:40:43 -0500

    migrate to vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx}",
-    "./src/*.{js,jsx}",
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
